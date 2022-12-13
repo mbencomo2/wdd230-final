ccde7b68b39da777845d9a56d96053d191676317
mbencomo2 <matt.ibencomo@gmail.com>
2022-12-12 23:54:05 -0700

    js for about page complete

---
+++ js/about.js
@@ -0,0 +1,12 @@
+//Hamburger Button
+document.getElementById('navBtn').addEventListener('click', () => {
+  document.getElementById('primaryNav').classList.toggle('open');
+  document.getElementById('navBtn').classList.toggle('open');
+})
+
+//Set the Last Modified Date
+document.getElementById('lastModified').innerText = document.lastModified;
+
+//Set the Copyright year
+const date = new Date();
+document.getElementById('copyright').textContent = date.getFullYear();
