a96258bf6f072bac1e25012b33c188d8e4bbc993
mbencomo2 <matt.ibencomo@gmail.com>
2022-12-12 23:52:00 -0700

    Reoganized and added comments

--- js/fresh.js
+++ js/fresh.js
@@ -1,63 +1,79 @@
 async function getfruit() {
+  //Fetch the data and await the response
   const response = await fetch(fruitURL);
   if (response.ok) {
+    //Store the response as a json object
     const fruits = await response.json();
     output(fruits);
   }
 };
 
 async function getNutrition(options) {
+  //Fetch the data and await the response
   const response = await fetch(fruitURL);
   if (response.ok) {
+    //Store the response as a json object
     const fruits = await response.json();
     displayOrder(fruits);
   }
 }
 
 function output(fruits) {
-  let num = 0;
-  while (num < 3) {
+  for (let i = 0; i < 3; i++) {
+    //Create the elements we need
     const label = document.createElement('label');
     const select = document.createElement('select');
-    const name = `fruit${num + 1}`
-
+    const name = `fruit${i + 1}`
+    //Set the name and id for the select element
     select.name = name;
     select.id = name;
-    label.textContent = `Fruit ${num + 1}: `
-    select.innerHTML = '<option value="Select a Fruit">Select a Fruit</option>'
+    //Set the label text and select options
+    label.textContent = `Fruit ${i + 1}: `
+    select.required = true;
+    select.innerHTML = '<option value="">Select a Fruit</option>'
     for (let index = 0; index < fruits.length; index++) {
       const fruit = fruits[index];
+      //Create the option element
       const option = document.createElement('option');
+      //Set the value and text content
       option.value = fruit.name;
       option.textContent = fruit.name;
+      //append it to the current select element
       select.appendChild(option);
     }
+    //append the label and select input to the fieldset
     label.appendChild(select);
     document.getElementById('fruitOptions').appendChild(label)
-    num += 1;
   }
 }
 
 function displayOrder(fruits) {
+  //Resets the order  to avoid duplicates
   document.getElementById('orderDetails').innerHTML = "";
+  //Create the elements we need
   const section = document.createElement('section');
   const order = document.createElement('div');
+  const date = document.createElement('dl')
   const contact = document.createElement('dl');
   const chosenFruit = document.createElement('dl');
   const nutrition = document.createElement('dl');
   const instructions = document.createElement('dl');
   const thanks = document.createElement('p');
-
+  //init variables for nutrition info
   let carbs = 0;
   let protein = 0;
   let fat = 0;
   let cal = 0;
   let sugar = 0;
-
+  //Create the header and set the id for styling
   section.innerHTML = '<h2>Order Details</h2>';
   order.id = 'order';
+  date.innerHTML = `<dt><b>Date</b></dt><dd>${new Date()}</dd>`
+  //Fill in Contact info from the form
   contact.innerHTML = `<dt><h3>Contact</h3></dt><dd><b>Email:</b> ${document.getElementById('email').value}</dd><dd><b>Name:</b> ${document.getElementById('firstName').value} ${document.getElementById('lastName').value}</dd>`
+  //Fill in the fruits selected in the form
   chosenFruit.innerHTML = `<dt><h3>Fruit Choices</h3></dt><dd><b>Fruit 1:</b> ${document.getElementById('fruit1').value}</dd><dd><b>Fruit 2:</b> ${document.getElementById('fruit2').value}</dd><dd><b>Fruit 3:</b> ${document.getElementById('fruit3').value}</dd>`
+  //For each fruit chosen, find it in our fetched data and update the nutrition info
   for (let index = 0; index < 3; index++) {
     const option = document.getElementById(`fruit${index + 1}`);
     for (let fruitI = 0; fruitI < fruits.length; fruitI++) {
@@ -71,11 +87,14 @@ function displayOrder(fruits) {
       }
     };
   };
+  //Fill in the nutrition info obtained
   nutrition.innerHTML = `<dt><h3>Nutrition</h3></dt><dd><b>Carbohydrates</b>: ${carbs.toFixed(2)}</dd><dd><b>Protein</b>: ${protein.toFixed(2)}</dd><dd><b>Fat</b>: ${fat.toFixed(2)}</dd><dd><b>Calories</b>: ${cal.toFixed(2)}</dd><dd><b>Sugar</b>: ${sugar.toFixed(2)}</dd>`
   instructions.innerHTML = `<dt><h3>Instructions</h3></dt><dd>${document.getElementById('addInstruct').value}</dd>`
+  //Create a message for the user
   thanks.innerHTML = 'Thank you for sharing your creativity with us! We will be contacting you via the email provided shortly (1-2 business days) with more details for payment and delivery.'
   thanks.id = 'thankYou'
-
+  //Append all the elements we created to the live document
+  order.appendChild(date)
   order.appendChild(contact);
   order.appendChild(chosenFruit);
   order.appendChild(nutrition);
@@ -83,6 +102,19 @@ function displayOrder(fruits) {
   order.appendChild(thanks);
   section.appendChild(order);
   document.getElementById('orderDetails').appendChild(section);
+  //Update the number of orders the customer has made in localStorage
+  storeOrder();
+}
+
+function storeOrder() {
+  const storage = localStorage;
+  if ('numOrders' in storage) {
+    const orders = storage.getItem('numOrders')
+    storage.setItem('numOrders', orders * 1 + 1)
+  }
+  else {
+    storage.setItem('numOrders', '1')
+  }
 }
 
 //Hamburger Button Listener
@@ -97,22 +129,18 @@ const fruitURL = "https://brotherblazzard.github.io/canvas-content/fruit.json";
 document.getElementById('copyright').textContent = date.getFullYear();
 getfruit();
 
-//Display help text when the form will not validate
-const form = document.querySelector('form');
-form.addEventListener("change", () => {
-  if (form.checkValidity()) {
-    document.getElementById('invalidForm').style = 'display:none;';
-    document.getElementById('submitBtn').disabled = !form.checkValidity();
-  }
-  else if (!form.checkValidity()) {
-    document.getElementById('invalidForm').style = 'display:block;';
-    document.getElementById('submitBtn').disabled = form.checkValidity();
-  }
-});
+//Set the Last Modified Date
+document.getElementById('lastModified').innerText = document.lastModified;
 
 //Event Listener for Submit Button
 document.getElementById('submitBtn').addEventListener('click', () => {
   if (form.checkValidity()) {
+    document.getElementById('invalidForm').style = 'display:none;';
+    document.getElementById('submitBtn').disabled = !form.checkValidity();
     getNutrition();
+  }
+  else if (!form.checkValidity()) {
+    document.getElementById('invalidForm').style = 'display:block;';
+    document.getElementById('submitBtn').disabled = form.checkValidity();
   }
 });
--- js/index.js
+++ js/index.js
@@ -1,39 +1,29 @@
-//Hamburger Button
-document.getElementById('navBtn').addEventListener('click', () => {
-  document.getElementById('primaryNav').classList.toggle('open');
-  document.getElementById('navBtn').classList.toggle('open');
-})
-//Load Weather Data
-const date = new Date();
-const key = '39bef332b3242a9fe5c82f1b824a81f9';
-const lat = 33.16;
-const lon = -117.33;
-const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`;
-const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`
-document.getElementById('copyright').textContent = date.getFullYear();
-getWeather();
-getForecast();
-
 async function getWeather() {
+  //Fetch the data and await the response
   const response = await fetch(weatherURL);
   if (response.ok) {
+    //Store the response as a json object
     const data = await response.json();
     outputWeather(data);
   }
 };
 
 async function getForecast() {
+  //Fetch the data and await the response
   const response = await fetch(forecastURL);
   if (response.ok) {
+    //Store the response as a json object
     const data = await response.json();
     outputForecast(data);
   }
 }
 
 function outputWeather(data) {
+  //Format the weather description to capitalize the words
   const words = data.weather[0].description.split(" ");
   const description = words.map((word) => { return word[0].toUpperCase() + word.substring(1) }).join(" ");
 
+  //Fill in elements with the data fetched
   document.getElementById('temperature').textContent = data.main.temp.toFixed(0);
   document.getElementById('description').textContent = description;
   document.getElementById('humidity').textContent = data.main.humidity;
@@ -41,33 +31,47 @@ function outputWeather(data) {
 }
 
 function outputForecast(data) {
+  //Index to keep track of what list entry
   let index = 0;
+  //Get today's date
   let day = date.getUTCDate();
+
+  //For each hour in the forecast data
   data.list.forEach((forecast) => {
+    //If index % 8 == 0 it is the next day since forecast lists data in 3 hour snapshots
     if (index % 8 == 0) {
+      //Create the elements we need
       const div = document.createElement('div');
       const heading = document.createElement('h4');
       const icon = document.createElement('img');
       const temp = document.createElement('p');
+      //Format the weather description to capitalize the words
       const words = forecast.weather[0].description.split(" ");
       const description = words.map((word) => { return word[0].toUpperCase() + word.substring(1) }).join(" ");
 
+      //Fill in the data and set the needed attributes
       heading.innerHTML = `${getMonth(date.getMonth())} ${day}`;
       temp.innerHTML = `${forecast.main.temp.toFixed(0)}&deg;F`;
       icon.setAttribute('src', `http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`);
       icon.setAttribute('alt', description);
       icon.setAttribute('loading', 'lazy');
+
+      //Append the elements we created to the HTML Document
       div.appendChild(heading);
       div.appendChild(icon);
       div.appendChild(temp);
       document.getElementById('forecasts').appendChild(div);
+
+      //Increment for the next day
       day += 1;
     }
+    //Update the index to keep track of where we are in the list
     index += 1;
   });
 };
 
 function getMonth(number) {
+  //Return the name of the month
   switch (number) {
     case 1:
       return 'January';
@@ -95,3 +99,39 @@ function getMonth(number) {
       return 'December';
   }
 }
+
+function getOrders() {
+  //Find numOrders in storage and retrieve its value, otherwise we return a string
+  if ('numOrders' in localStorage) {
+    return `${localStorage.getItem('numOrders')} Drink(s)`
+  }
+  else {
+    return 'No orders yet!'
+  }
+}
+
+//Hamburger Button
+document.getElementById('navBtn').addEventListener('click', () => {
+  document.getElementById('primaryNav').classList.toggle('open');
+  document.getElementById('navBtn').classList.toggle('open');
+})
+
+//Set the number of drinks ordered from local storage
+document.getElementById('drinks').innerText = getOrders();
+
+//Set the Copyright year
+const date = new Date();
+document.getElementById('copyright').textContent = date.getFullYear();
+
+//Set the Last Modified Date
+document.getElementById('lastModified').innerText = document.lastModified;
+
+//Set all the data we need to load weather data
+const key = '39bef332b3242a9fe5c82f1b824a81f9';
+const lat = 33.16;
+const lon = -117.33;
+const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`;
+const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`
+//Load Weather Data
+getWeather();
+getForecast();
